bb4c0c14238c7e1b9c05ff7a97c725e98bb0fcd5
rpanagides <108998966+rpanagides@users.noreply.github.com>
2023-07-31 10:52:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='package_name',
       version='0.1',
       description='Function to add two numbers',
-      url='http://github.com/rpanagides/module_nine_repo/package_name/module_nine',
+      url='http://github.com/rpanagides/module_nine_repo',
       author='Reanna Panagides',
       author_email='reannapanagides@gmail.com',
       license='MIT',
